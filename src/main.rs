1d100362efc5c9b5da7b4815061463ea7da9ed66
marius851000 <mariusdavid@laposte.net>
2020-03-16 10:13:47 +0100

    added an example of mod

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use pmd_flow::FlowData;
+use pmd_flow::{FlowData, FlowDataValue};
 use std::fs::File;
 use std::path::PathBuf;
 
@@ -6,10 +6,13 @@ fn main() {
     let input_path = PathBuf::from("./script_flow_data_us.bin");
     let input_file = File::open(&input_path).unwrap();
 
-    let flow = FlowData::new(input_file).unwrap();
+    let mut flow = FlowData::new(input_file).unwrap();
     //Some hacky hack...
-    //let dic4 = flow.get_dictionary_mut(4).unwrap();
-    //dic4.insert("to".into(), FlowDataValue::String("$0x572F".into()));
+    let dic4 = flow.get_dictionary_mut(4).unwrap();
+    dic4.insert("to".into(), FlowDataValue::String("$0x000A".into()));
+    let dic192 = flow.get_dictionary_mut(192).unwrap();
+    dic192.insert("map".into(), FlowDataValue::String("FM_TW00_SCHOOL00".into()));
+    dic192.insert("place".into(), FlowDataValue::String("EV_SCHOOL00_START_POINT02".into()));
     let output_path = PathBuf::from("./script_handmade.bin");
     {
         let mut output_file = File::create(&output_path).unwrap();
