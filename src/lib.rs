0a93295a5072cb9f97bf456ac3e9dc599549d8b0
marius david <mariusdavid@laposte.net>
2020-05-23 14:55:14 +0200

    add crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! This library can read flow file, as used in Pokemon Super Mystery Dungeon.
+//!
+//! While it can read and write the script_flow_data_us.bin file used in this game, this library
+//! isn't finished. It doesn't support different flow file used in this game, and there is many
+//! assertion that crash the program rather than returning Error.
 mod flowdata;
 pub use flowdata::{FlowData, FlowDataError, FlowDataValue};
 
